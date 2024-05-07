295a9738c30d40a064f689a807b5691919765ec1
Victor Castell <0x@vcastellm.xyz>
2024-05-07 16:59:33 +0000

    Non erroring

--- src/stream_client.rs
+++ src/stream_client.rs
@@ -1,76 +1,116 @@
+use std::io::{Read, Write};
 use std::net::TcpStream;
-use std::time::Duration;
 use std::thread;
+use std::time::Duration;
+use thiserror::Error;
+use std::io::{self, ErrorKind};
+use byteorder::{ByteOrder, BigEndian};
+use std::convert::From;
 
 // Buffers for the channels
 const RESULTS_BUFFER: usize = 32;
 const HEADERS_BUFFER: usize = 32;
 const ENTRIES_BUFFER: usize = 128;
 const ENTRY_RSP_BUFFER: usize = 32;
+const HEADER_SIZE: usize = 38;
+const FIXED_SIZE_FILE_ENTRY: usize = 17;
 
 // Entry type for a data file entry
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct Entry {
-    packet_type: u8,     // 2:Data entry, 0:Padding
-    length: u32,    // Total length of the entry (17 bytes + length(data))
+    packet_type: u8,       // 2:Data entry, 0:Padding
+    length: u32,           // Total length of the entry (17 bytes + length(data))
     entry_type: EntryType, // 0xb0:Bookmark, 1:Event1, 2:Event2,...
-    number: u64,    // Entry number (sequential starting with 0)
+    number: u64,           // Entry number (sequential starting with 0)
     data: Vec<u8>,
 }
 
 // HeaderEntry type for a header entry
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct HeaderEntry {
-    packet_type: u8,      // 1:Header
-    head_length: u32,     // Total length of header entry (38)
-    version: u8,          // Stream file version
-    system_id: u64,       // System identifier (e.g. ChainID)
+    packet_type: u8,         // 1:Header
+    head_length: u32,        // Total length of header entry (38)
+    version: u8,             // Stream file version
+    system_id: u64,          // System identifier (e.g. ChainID)
     stream_type: StreamType, // 1:Sequencer
-    total_length: u64,     // Total bytes used in the file
-    total_entries: u64,     // Total number of data entries (packet type PtData)
+    total_length: u64,       // Total bytes used in the file
+    total_entries: u64,      // Total number of data entries (packet type PtData)
 }
 
 // EntryType enum represents the entry event types
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Default, PartialEq)]
 pub enum EntryType {
-    EntryTypeNotFound = 0, // EntryTypeNotFound for entry not found
-    EntryTypeBookmark = 0xb0, // EntryTypeBookmark for bookmark entry
-    EntryTypeEvent1 = 1, // EntryTypeEvent1 for event type 1
-    EntryTypeEvent2 = 2, // EntryTypeEvent2 for event type 2
+    #[default]
+    NotFound = 0,    // EntryTypeNotFound for entry not found
+    Bookmark = 0xb0, // EntryTypeBookmark for bookmark entry
+    Event1 = 1,      // EntryTypeEvent1 for event type 1
+    Event2 = 2,      // EntryTypeEvent2 for event type 2
+}
+
+impl From<u32> for EntryType {
+    fn from(v: u32) -> Self {
+        match v {
+            0 => EntryType::NotFound,
+            0xb0 => EntryType::Bookmark,
+            1 => EntryType::Event1,
+            2 => EntryType::Event2,
+            _ => EntryType::NotFound,
+        }
+    }
 }
 
 #[derive(Debug, Clone, Copy)]
 pub enum Command {
-    CmdStart = 1, // CmdStart for the start from entry TCP client command
-    CmdStop, // CmdStop for the stop TCP client command
-    CmdHeader, // CmdHeader for the header TCP client command
+    CmdStart = 1,     // CmdStart for the start from entry TCP client command
+    CmdStop,          // CmdStop for the stop TCP client command
+    CmdHeader,        // CmdHeader for the header TCP client command
     CmdStartBookmark, // CmdStartBookmark for the start from bookmark TCP client command
-    CmdEntry, // CmdEntry for the get entry TCP client command
-    CmdBookmark, // CmdBookmark for the get bookmark TCP client command
+    CmdEntry,         // CmdEntry for the get entry TCP client command
+    CmdBookmark,      // CmdBookmark for the get bookmark TCP client command
 }
 
 #[derive(Debug, Clone, Copy)]
 pub enum CommandError {
-    CmdErrOK = 0, // CmdErrOK for no error
-    CmdErrAlreadyStarted, // CmdErrAlreadyStarted for client already started error
-    CmdErrAlreadyStopped, // CmdErrAlreadyStopped for client already stopped error
-    CmdErrBadFromEntry, // CmdErrBadFromEntry for invalid starting entry number
-    CmdErrBadFromBookmark, // CmdErrBadFromBookmark for invalid starting bookmark
+    CmdErrOK = 0,             // CmdErrOK for no error
+    CmdErrAlreadyStarted,     // CmdErrAlreadyStarted for client already started error
+    CmdErrAlreadyStopped,     // CmdErrAlreadyStopped for client already stopped error
+    CmdErrBadFromEntry,       // CmdErrBadFromEntry for invalid starting entry number
+    CmdErrBadFromBookmark,    // CmdErrBadFromBookmark for invalid starting bookmark
     CmdErrInvalidCommand = 9, // CmdErrInvalidCommand for invalid/unknown command error
 }
 
 // StreamType enum represents the stream types
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Default, PartialEq)]
 pub enum StreamType {
+    #[default]
     Sequencer = 1, // Sequencer for sequencer stream type
 }
 
-// Type of the callback function to process the received entry
-type ProcessEntryFunc = fn(Entry) -> Result<(), Box<dyn std::error::Error>>;
+impl From<u64> for StreamType {
+    fn from(v: u64) -> Self {
+        match v {
+            1 => StreamType::Sequencer,
+            _ => StreamType::Sequencer,
+        }
+    }
+}
 
-enum ClientError {
-    ClientNotStarted,
-    InvalidCommand,
+// Type of the callback function to process the received entry
+type ProcessEntryFunc = fn(Entry) -> Result<(), ClientError>;
+
+// ClientError enum represents the client errors
+#[derive(Debug, Error)]
+pub enum ClientError {
+    #[error("{0} Client not started")]
+    ClientNotStarted(&'static str),
+    #[error("Error executing command: {0}")]
+    InvalidCommand(&'static str),
+    #[error("Error network")]
+    NetworkError(std::io::Error),
+    #[error("Errors entry not found")]
+    EntryNotFound,
+    #[error("Error bookmark not found")]
+    BookmarkNotFound,
 }
 
 #[derive(Debug)]
@@ -91,13 +131,11 @@ pub struct StreamClient {
 }
 
 impl StreamClient {
-    pub fn new(
-        server: String,
-    ) -> Result<StreamClient, Box<dyn std::error::Error>> {
+    pub fn new(server: String) -> Result<StreamClient, Box<dyn std::error::Error>> {
         let client = StreamClient {
-            server,
+            server: server.clone(),
             stream_type: StreamType::Sequencer,
-            conn: TcpStream::connect(&server)?,
+            conn: TcpStream::connect(server.clone())?,
             id: String::new(),
             started: false,
             connected: false,
@@ -134,12 +172,12 @@ impl StreamClient {
 
                     // Restore streaming
                     if self.streaming {
-                        match self.exec_command(Command::CmdStart, true, 0, None) {
+                        match self.exec_command(Command::CmdStart, 0, None) {
                             Ok(_) => {}
                             Err(e) => {
                                 self.close_connection();
                                 thread::sleep(Duration::from_secs(5));
-                                println!("Error restoring streaming: {}", e);
+                                println!("Error restoring streaming: {:?}", e);
                                 self.streaming = false;
                                 continue;
                             }
@@ -177,57 +215,48 @@ impl StreamClient {
     }
 
     // exec_command_start executes client TCP command to start streaming from entry
-    pub fn exec_command_start(&self, from_entry: u64) -> Result<(), Box<dyn std::error::Error>> {
-        match self.exec_command(Command::CmdStart, false, from_entry, None) {
+    pub fn exec_command_start(&mut self, from_entry: u64) -> Result<(), ClientError> {
+        match self.exec_command(Command::CmdStart, from_entry, None) {
             Ok(_) => Ok(()),
             Err(e) => Err(e),
         }
     }
 
     // exec_command_start_bookmark executes client TCP command to start streaming from bookmark
-    pub fn exec_command_start_bookmark(
-        &self,
-        from_bookmark: Vec<u8>,
-    ) -> Result<(), Box<dyn std::error::Error>> {
-        match self.exec_command(Command::CmdStartBookmark, false, 0, Some(from_bookmark)) {
+    pub fn exec_command_start_bookmark(&mut self, from_bookmark: Vec<u8>) -> Result<(), ClientError> {
+        match self.exec_command(Command::CmdStartBookmark, 0, Some(from_bookmark)) {
             Ok(_) => Ok(()),
             Err(e) => Err(e),
         }
     }
 
     // exec_command_stop executes client TCP command to stop streaming
-    pub fn exec_command_stop(&self) -> Result<(), Box<dyn std::error::Error>> {
-        match self.exec_command(Command::CmdStop, false, 0, None) {
+    pub fn exec_command_stop(&mut self) -> Result<(), ClientError> {
+        match self.exec_command(Command::CmdStop, 0, None) {
             Ok(_) => Ok(()),
             Err(e) => Err(e),
         }
     }
 
     // exec_command_get_header executes client TCP command to get the header
-    pub fn exec_command_get_header(&self) -> Result<HeaderEntry, Box<dyn std::error::Error>> {
-        match self.exec_command(Command::CmdHeader, false, 0, None) {
+    pub fn exec_command_get_header(&mut self) -> Result<HeaderEntry, ClientError> {
+        match self.exec_command(Command::CmdHeader, 0, None) {
             Ok((header, _)) => Ok(header),
             Err(e) => Err(e),
         }
     }
 
     // exec_command_get_entry executes client TCP command to get an entry
-    pub fn exec_command_get_entry(
-        &self,
-        from_entry: u64,
-    ) -> Result<Entry, Box<dyn std::error::Error>> {
-        match self.exec_command(Command::CmdEntry, false, from_entry, None) {
+    pub fn exec_command_get_entry(&mut self, from_entry: u64) -> Result<Entry, ClientError> {
+        match self.exec_command(Command::CmdEntry, from_entry, None) {
             Ok((_, entry)) => Ok(entry),
             Err(e) => Err(e),
         }
     }
 
     // exec_command_get_bookmark executes client TCP command to get a bookmark
-    pub fn exec_command_get_bookmark(
-        &self,
-        from_bookmark: Vec<u8>,
-    ) -> Result<Entry, Box<dyn std::error::Error>> {
-        match self.exec_command(Command::CmdBookmark, false, 0, Some(from_bookmark)) {
+    pub fn exec_command_get_bookmark(&mut self, from_bookmark: Vec<u8>) -> Result<Entry, ClientError> {
+        match self.exec_command(Command::CmdBookmark, 0, Some(from_bookmark)) {
             Ok((_, entry)) => Ok(entry),
             Err(e) => Err(e),
         }
@@ -237,85 +266,79 @@ impl StreamClient {
     fn exec_command(
         &mut self,
         cmd: Command,
-        deferred_result: bool,
         from_entry: u64,
         from_bookmark: Option<Vec<u8>>,
     ) -> Result<(HeaderEntry, Entry), ClientError> {
-        println!(
-            "{} Executing command {:?}...",
-            self.id,
-            cmd,
-        );
-        let mut header: HeaderEntry;
-        let mut entry: Entry;
+        println!("{} Executing command {:?}...", self.id, cmd,);
+        let mut header: HeaderEntry = Default::default();
+        let mut entry: Entry = Default::default();
 
         // Check status of the client
         if !self.started {
             println!("Execute command not allowed. Client is not started");
-            return Err(Box::new(Error::new(
-                ErrorKind::Other,
-                "Execute command not allowed. Client is not started",
-            )));
-        }
-
-        // Check valid command
-        if !cmd.is_a_command() {
-            println!("{} Invalid command {}", self.id, cmd);
-            return Err(Box::new(Error::new(ErrorKind::Other, "Invalid command")));
+            return Err(ClientError::ClientNotStarted(
+                "Execute command not allowed.",
+            ));
         }
 
         // Send command
-        self.conn.write_all(&(cmd as u64).to_le_bytes())?;
+        self.conn
+            .write_all(&(cmd as u64).to_le_bytes())
+            .expect("Error sending command");
 
         // Send stream type
         self.conn
-            .write_all(&(self.stream_type as u64).to_le_bytes())?;
+            .write_all(&(self.stream_type as u64).to_le_bytes())
+            .expect("Error sending stream type");
 
         // Send the command parameters
         match cmd {
             Command::CmdStart => {
                 println!("{} ...from entry {}", self.id, from_entry);
                 // Send starting/from entry number
-                self.conn.write_all(&from_entry.to_le_bytes())?;
+                self.conn
+                    .write_all(&from_entry.to_le_bytes())
+                    .expect("Error sending from entry");
             }
             Command::CmdStartBookmark => {
                 println!("{} ...from bookmark {:?}", self.id, from_bookmark);
                 // Send starting/from bookmark length
                 if let Some(bookmark) = &from_bookmark {
                     self.conn
-                        .write_all(&(bookmark.len() as u32).to_le_bytes())?;
+                        .write_all(&(bookmark.len() as u32).to_le_bytes())
+                        .expect("Error sending from bookmark length");
                     // Send starting/from bookmark
-                    self.conn.write_all(bookmark)?;
+                    self.conn
+                        .write_all(bookmark)
+                        .expect("Error sending from bookmark");
                 }
             }
             Command::CmdEntry => {
                 println!("{} ...get entry {}", self.id, from_entry);
                 // Send entry to retrieve
-                self.conn.write_all(&from_entry.to_le_bytes())?;
+                self.conn
+                    .write_all(&from_entry.to_le_bytes())
+                    .expect("Error sending entry");
             }
             Command::CmdBookmark => {
                 println!("{} ...get bookmark {:?}", self.id, from_bookmark);
                 // Send bookmark length
                 if let Some(bookmark) = &from_bookmark {
                     self.conn
-                        .write_all(&(bookmark.len() as u32).to_le_bytes())?;
+                        .write_all(&(bookmark.len() as u32).to_le_bytes())
+                        .expect("Error sending bookmark length");
                     // Send bookmark to retrieve
-                    self.conn.write_all(bookmark)?;
+                    self.conn
+                        .write_all(bookmark)
+                        .expect("Error sending bookmark");
                 }
             }
             _ => {}
         }
 
         // Get the command result
-        if !deferred_result {
-            let r = self.get_result(cmd);
-            if r.error_num != Command::CmdErrOK as u32 {
-                return Err(Box::new(Error::new(
-                    ErrorKind::Other,
-                    "Result command error",
-                )));
-            }
-        }
+        let mut buf = vec![0u8; ENTRY_RSP_BUFFER];
+        self.conn.read_to_end(&mut buf).expect("Error reading response");
 
         // Get the data response and update streaming flag
         match cmd {
@@ -330,46 +353,97 @@ impl StreamClient {
                 self.streaming = false;
             }
             Command::CmdHeader => {
-                let h = self.get_header();
+                let h = decode_binary_to_header_entry(&mut buf).expect("Error decoding header");
                 header = h;
                 self.total_entries = header.total_entries;
             }
             Command::CmdEntry => {
-                let e = self.get_entry();
-                if e.entry_type == EntryType::EntryTypeNotFound {
-                    return Err(Box::new(Error::new(ErrorKind::Other, "Entry not found")));
+                let e = decode_binary_to_file_entry(&mut buf).expect("Error decoding entry");
+                if e.entry_type == EntryType::NotFound {
+                    return Err(ClientError::EntryNotFound)
                 }
                 entry = e;
             }
             Command::CmdBookmark => {
-                let e = self.get_entry();
-                if e.entry_type == EntryType::EntryTypeNotFound {
-                    return Err(Box::new(Error::new(ErrorKind::Other, "Bookmark not found")));
+                let e = decode_binary_to_file_entry(&mut buf).expect("Error decoding bookmark");
+                if e.entry_type == EntryType::NotFound {
+                     return Err(ClientError::BookmarkNotFound)
                 }
                 entry = e;
             }
-            _ => {}
         }
 
         Ok((header, entry))
     }
 }
 
-fn print_received_entry(entry: Entry) -> Result<(), Box<dyn std::error::Error>> {
+// decode_binary_to_header_entry decodes from binary bytes slice to a header entry type
+fn decode_binary_to_header_entry(b: &[u8]) -> io::Result<HeaderEntry> {
+    if b.len() != HEADER_SIZE {
+        return Err(io::Error::new(ErrorKind::InvalidData, "Invalid binary header entry"));
+    }
+
+    let packet_type = b[0];
+    let head_length = BigEndian::read_u32(&b[1..5]);
+    let version = b[5];
+    let system_id = BigEndian::read_u64(&b[6..14]);
+    let stream_type = StreamType::from(BigEndian::read_u64(&b[14..22])); // Convert u64 to StreamType
+    let total_length = BigEndian::read_u64(&b[22..30]);
+    let total_entries = BigEndian::read_u64(&b[30..38]);
+
+    Ok(HeaderEntry {
+        packet_type,
+        head_length,
+        version,
+        system_id,
+        stream_type,
+        total_length,
+        total_entries,
+    })
+}
+
+// decode_binary_to_file_entry decodes from binary bytes slice to file entry type
+fn decode_binary_to_file_entry(b: &[u8]) -> io::Result<Entry> {
+    if b.len() < FIXED_SIZE_FILE_ENTRY {
+        return Err(io::Error::new(ErrorKind::InvalidData, "Invalid binary data entry"));
+    }
+
+    let packet_type = b[0];
+    let length = BigEndian::read_u32(&b[1..5]);
+    let entry_type = EntryType::from(BigEndian::read_u32(&b[5..9])); // Convert u32 to EntryType
+    let number = BigEndian::read_u64(&b[9..17]);
+    let data = b[17..].to_vec();
+
+    if data.len() as u32 != length - FIXED_SIZE_FILE_ENTRY as u32 {
+        return Err(io::Error::new(ErrorKind::InvalidData, "Error decoding binary data entry"));
+    }
+
+    Ok(Entry {
+        packet_type,
+        length,
+        entry_type,
+        number,
+        data,
+    })
+}
+
+fn print_received_entry(entry: Entry) -> Result<(), ClientError> {
     println!("Received entry: {:?}", entry);
     Ok(())
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-//     #[test]
-//     fn test_stream_client_new() {
-//         let server = "localhost:6000".to_string();
-//         let stream_type = StreamType::Sequencer;
-//         let client = StreamClient::new(server, stream_type).unwrap();
-//         assert_eq!(client.server, server);
-//         assert_eq!(client.stream_type, stream_type);
-//     }
-// }
+    #[tokio::test]
+    async fn test_stream_client_new() {
+        let server = "stream.zkevm-rpc.com:6900".to_string();
+        let stream_type = StreamType::Sequencer;
+        let mut client = StreamClient::new(server.clone()).unwrap();
+        assert_eq!(client.server, server);
+        assert_eq!(client.stream_type, stream_type);
+
+        client.start().await.unwrap();
+    }
+}
