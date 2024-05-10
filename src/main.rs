ed9cf4b702d01df5876b5f4016d673b8c4a0a885
Victor Castell <0x@vcastellm.xyz>
2024-05-10 13:15:35 +0000

    Typo

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,5 @@ mod stream_client;
 async fn main() {
     let server = stream_client::StreamClient::new("localhost:6900".to_string());
 
-    _ = server.expect("Failt to create").start().await;
+    _ = server.expect("Failed to create").start().await;
 }
